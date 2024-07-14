3028b27646cf6ee2f44a662865f29edaa198f7e1
filiperak <filip.erak@googlemail.com>
2024-07-14 15:38:59 +0200

    db conection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,13 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+
+//database
+const MONGO_URL = 'mongodb+srv://filiperak:filiperak@e-commerce.xggmbpa.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce'
+mongoose.connect(MONGO_URL)
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
 
 const app = express();
 app.use(express.json());
