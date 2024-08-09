4d57ddd3f4486215d34543e07e8e77c50856d050
filiperak <filip.erak@googlemail.com>
2024-08-09 18:31:10 +0200

    usermodel update

--- server/models/users.ts
+++ server/models/users.ts
@@ -4,16 +4,30 @@ interface IUser extends Document {
     username:string,
     email:string,
     password:string,
-    createdJobs:mongoose.Types.ObjectId[],
-    appliedJobs:mongoose.Types.ObjectId[],
 }
 
 const UsersSchema:Schema<IUser> = new Schema ({
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    createdJobs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Job' }],
-    appliedJobs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Job' }]
+    username:{
+        type:String,
+        required:[true,'provide name'],
+        minlength:3,
+        maxlength:30,
+    },
+    email:{
+        type:String,
+        required:[true,'provide name'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please provide a valid email',
+          ],
+          unique: true,
+    },
+    
+    password:{
+        type:String,
+        required:[true,'provide password'],
+        minlength:3,
+    }
 })
 
 export const UserModel:Model<IUser> = mongoose.model('User',UsersSchema)
