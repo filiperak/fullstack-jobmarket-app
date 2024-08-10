92513212ada0f842f93088a073c5461e21c74293
filiperak <filip.erak@googlemail.com>
2024-08-10 15:20:10 +0200

    login pop up

--- client/src/components/loginModal/LoginModal.tsx
+++ client/src/components/loginModal/LoginModal.tsx
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { ISidebar } from "../../interface/props";
 import styles from "../../styles/modal.module.css";
 
-const LoginModal = ({ open,setModalOpen }: ISidebar) => {
+const LoginModal = ({ open, setModalOpen }: ISidebar) => {
+  const [register, setRegister] = useState<boolean>(false);
+
   if (!open) return null;
+
+  const handleToggle = () => {
+    setRegister((prev) => !prev);
+  };
+
+  const handleClose = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setModalOpen();
+  };
+
   return (
     <>
       <div className={styles.overlay} />
       <form className={styles.modal}>
-        <h3>Please log in</h3>
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/>
-
-
+        <h3>{register ? "Please register account" : "Please sign in"}</h3>
+        <p>
+          {register ? "Already have an account?" : "Don't have an account?"}{" "}
+          <span className={styles.signinSpan} onClick={handleToggle}>
+            {register ? "Sign In" : "Register"}
+          </span>
+        </p>
+        <input type="text" placeholder="username" />
+        {register && <input type="text" placeholder="email" />}
+        <input type="password" placeholder="password" />
 
         <div className={styles.btnContainer}>
-          <button className={styles.cancelBtn} onClick={() => setModalOpen()}>cancel</button>
-          <button className={styles.confirmBtn}>register</button>
+          <button className={styles.cancelBtn} onClick={handleClose}>
+            Cancel
+          </button>
+          <button className={styles.confirmBtn} type="submit">
+            {register ? "Register" : "Sign in"}
+          </button>
         </div>
       </form>
     </>
