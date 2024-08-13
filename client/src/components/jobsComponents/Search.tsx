4e695b3bf5a1d785897a2c581f8de38a7dfccd6a
filiperak <filip.erak@googlemail.com>
2024-08-13 15:04:19 +0200

    search bar style

--- client/src/components/jobsComponents/Search.tsx
+++ client/src/components/jobsComponents/Search.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import styles from '../../styles/jobs.module.css'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import TuneOutlinedIcon from '@mui/icons-material/TuneOutlined';
+
 const Search = () => {
   return (
-    <div className={styles.search}>Search</div>
+    <form className={styles.search}>
+      <div className={styles.barAndFilter}>
+      <div className={styles.bar}>
+        <SearchOutlinedIcon/>
+        <input type="text" name="" id=""  placeholder='Search jobs...'/>
+        <button type='submit'>Search</button>
+      </div>
+      <div className={styles.filter}>
+        <TuneOutlinedIcon/>
+      </div>
+      </div>
+    </form>
   )
 }
 
