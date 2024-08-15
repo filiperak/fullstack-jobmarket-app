2320babca602e61d91582e9fbb6b03ae58ae0c2c
filiperak <filip.erak@googlemail.com>
2024-08-15 15:56:00 +0200

    service apply to job

---
+++ client/src/services/jobs/applyToJob.ts
@@ -0,0 +1,19 @@
+import { API_URL } from "../API"
+
+export const applyToJon = async(token:string,params:string) => {
+    try {
+        const response = await fetch(`${API_URL}/jobs/${params}`,{
+            method: "PATCH",
+            headers:{
+                "Content-Type":"application/json",
+                Authorization: `Berer ${token}`,
+            }
+        })
+        const result = await response.json()
+        if(!response.ok){
+            throw new Error(result.message || "failed to apply to job")
+        }
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
+}
